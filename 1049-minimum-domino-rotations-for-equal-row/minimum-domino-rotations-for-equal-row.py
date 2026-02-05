805c8c3ef4b0ae3572817a3f7016862444c5eb65
Jainil2 <96487333+Jainil2@users.noreply.github.com>
2026-02-05 21:22:30 +0530

    Time: 19 ms (94.20%) | Memory: 20.6 MB (10.43%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.py
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.py
@@ -1,16 +1,15 @@
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-        idx = defaultdict(set)
-        freq = defaultdict(int)
-        freq2 = defaultdict(int)
-        n = len(tops)
-        for i in range(n):
-            idx[tops[i]].add(i)
-            freq[tops[i]] += 1
-        for i in range(n):
-            idx[bottoms[i]].add(i)
-            freq2[bottoms[i]] += 1
-        for i, _ in idx.items():
-            if len(idx[i]) == len(tops):
-                return min(freq[i], min(n - freq[i], min(n - freq2[i], freq2[i])))
-        return -1
+        def rotations(x):
+            top_rot = bottom_rot = 0
+            for t, b in zip(tops, bottoms):
+                if t != x and b != x:
+                    return float('inf')
+                if t != x:
+                    top_rot += 1
+                if b != x:
+                    bottom_rot += 1
+            return min(top_rot, bottom_rot)
+
+        res = min(rotations(tops[0]), rotations(bottoms[0]))
+        return res if res != float('inf') else -1
