77e0fbe6e58a0c8132bd76582faac049452afcf1
Jainil2 <96487333+Jainil2@users.noreply.github.com>
2026-02-06 23:26:12 +0530

    Time: 151 ms (52.71%) | Memory: 34.9 MB (6.86%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.py
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.py
@@ -1,7 +1,5 @@
 class Solution:
     def minRemoval(self, nums: List[int], k: int) -> int:
-        # if len(nums) <= 2:
-        #     return 0
         n = len(nums)
         nums.sort()
         i, j = 0, 0
