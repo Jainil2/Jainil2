9fcb6526024b965577d0970b995ba65eaed8a9e6
Jainil2 <96487333+Jainil2@users.noreply.github.com>
2026-03-30 12:57:25 +0530

    Time: 28 ms (91.77%) | Memory: 22.1 MB (81.93%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.py
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -1,26 +1,22 @@
+from bisect import bisect_right
+
 class Solution:
-    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        cap_prof = defaultdict(int)
-        merged = [[0,0] for _ in range(len(difficulty))]
-
-        for i in range(len(profit)):
-            merged[i] = [difficulty[i], profit[i]]
-
-        merged.sort()
-        difficulty.sort()
-
-        max_till_now = merged[0][1] 
-
-        for i in range(len(profit)):
-            max_till_now = max(max_till_now, merged[i][1])
-            cap_prof[merged[i][0]] = max_till_now
-
-        result = 0
+    def maxProfitAssignment(self, difficulty, profit, worker):
+        jobs = sorted(zip(difficulty, profit))
         
-        for i in range(len(worker)):
-            idx = bisect_right(difficulty, worker[i])
-            if idx == 0:
-                continue
-            result += cap_prof[difficulty[idx - 1]]
+        max_profit = 0
+        diffs = []
+        profits = []
+        
+        for d, p in jobs:
+            max_profit = max(max_profit, p)
+            diffs.append(d)
+            profits.append(max_profit)
+        
+        result = 0
+        for w in worker:
+            idx = bisect_right(diffs, w) - 1
+            if idx >= 0:
+                result += profits[idx]
         
         return result
