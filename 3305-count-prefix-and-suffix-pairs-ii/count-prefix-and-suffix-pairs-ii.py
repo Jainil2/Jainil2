c6091482acbd55e775bb0ad57886ef42c88c16e3
Jainil2 <96487333+Jainil2@users.noreply.github.com>
2026-05-06 10:38:09 +0530

    Time: 1096 ms (48.15%) | Memory: 236.8 MB (24.45%) - LeetSync

---
+++ 3305-count-prefix-and-suffix-pairs-ii/count-prefix-and-suffix-pairs-ii.py
@@ -0,0 +1,33 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+
+
+class Solution:
+
+    def countPrefixSuffixPairs(self, words):
+
+        root = TrieNode()
+
+        ans = 0
+
+        for word in words:
+
+            node = root
+            n = len(word)
+
+            for i in range(n):
+
+                key = (word[i], word[n - 1 - i])
+
+                if key not in node.children:
+                    node.children[key] = TrieNode()
+
+                node = node.children[key]
+
+                ans += node.count
+
+            node.count += 1
+
+        return ans
