4e624e540ad96a50f9e71424b5db1a529043844e
Jainil2 <96487333+Jainil2@users.noreply.github.com>
2026-05-31 09:51:25 +0530

    Time: 71 ms (73.70%) | Memory: 34.1 MB (30.13%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.py
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -1,11 +1,10 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
         asteroids.sort()
-        while asteroids:
-            idx = bisect.bisect_right(asteroids, mass) - 1
-            if idx >= 0 and asteroids[idx] <= mass:
-                mass += asteroids[idx]
-            else:
-                break
-            del asteroids[idx]
-        return len(asteroids) == 0
+
+        for asteroid in asteroids:
+            if mass < asteroid:
+                return False
+            mass += asteroid
+
+        return True
