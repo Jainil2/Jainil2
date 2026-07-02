58728763054312ef5b331fac6a73eaf3be38f1c9
Jainil2 <96487333+Jainil2@users.noreply.github.com>
2026-07-02 17:59:52 +0530

    Time: 52 ms (91.04%) | Memory: 19.4 MB (97.17%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
@@ -1,29 +1,34 @@
 class Solution:
     def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
         m, n = len(grid), len(grid[0])
-        h = []
-        dir = [[1, 0], [0, -1], [-1, 0], [0, 1]]
-        vis = [[-1] * n for _ in range(m)]
-        if grid[0][0] == 1:
-            heapq.heappush(h, (-health + 1, 0, 0))
-            vis[0][0] = health - 1
-        else:
-            heapq.heappush(h, (-health, 0, 0))
-            vis[0][0] = health
-        while h:
-            health, x, y = heapq.heappop(h)
-            if x == m - 1 and y == n - 1 and health < 0:
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        dis = [[float("inf")] * n for _ in range(m)]
+
+        q = deque()
+        q.appendleft((0, 0))
+        dis[0][0] = grid[0][0]
+
+        while q:
+            cx, cy = q.popleft()
+            # the first time it leaves the queue, the shortest distance is guaranteed
+            if cx == m - 1 and cy == n - 1:
                 return True
-            for dx, dy in dir:
-                nx = x + dx
-                ny = y + dy
-                if 0 <= nx < m and 0 <= ny < n:
-                    next_health = None
-                    if grid[nx][ny] == 1:
-                        next_health = -health - 1
+
+            for dx, dy in dirs:
+                nx, ny = cx + dx, cy + dy
+                if nx < 0 or ny < 0 or nx >= m or ny >= n:
+                    continue
+
+                cost = dis[cx][cy] + grid[nx][ny]
+                # pruning: the new distance does not meet health requirements
+                if cost >= health:
+                    continue
+
+                if cost < dis[nx][ny]:
+                    dis[nx][ny] = cost
+                    if grid[nx][ny] == 0:
+                        q.appendleft((nx, ny))
                     else:
-                        next_health = -health
-                    if next_health > vis[nx][ny]:
-                        vis[nx][ny] = next_health
-                        heapq.heappush(h, (-next_health, nx, ny))
+                        q.append((nx, ny))
+
         return False
