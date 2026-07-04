630b660089c2a17cbae304692607a4aaf9f01dd8
Jainil2 <96487333+Jainil2@users.noreply.github.com>
2026-07-04 14:25:41 +0530

    Time: 187 ms (43.74%) | Memory: 70.7 MB (75.75%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -2,23 +2,20 @@ class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         graph = defaultdict(list)
         for u, v, w in roads:
-            graph[u].append([v, w])
-            graph[v].append([u, w])
-        
-        dist = [float('inf')] * (n + 1)
-        pq = []
-        heapq.heappush(pq, (0, 1))
-        dist[1] = 0
+            graph[u].append((v, w))
+            graph[v].append((u, w))
+
+        visited = [False] * (n + 1)
+        visited[1] = True
+        stack = [1]
         result = float('inf')
-        vis = [False] * (n + 1)
-        while pq:
-            curr, node = heapq.heappop(pq)
-            # print(curr, node, pq)
+
+        while stack:
+            node = stack.pop()
             for adj, w in graph[node]:
-                if not vis[adj]:
-                    dist[adj] = curr + w
-                    result = min(result, w)
-                    # print(result, w)
-                    heapq.heappush(pq, (dist[adj], adj))
-            vis[node] = True
-        return -1 if result == float('inf') else result
+                result = min(result, w)
+                if not visited[adj]:
+                    visited[adj] = True
+                    stack.append(adj)
+
+        return result
