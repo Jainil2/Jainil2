9450be1b4717c3a3e02c2b450f0db9f32cc46e72
Jainil2 <96487333+Jainil2@users.noreply.github.com>
2026-07-29 16:08:34 +0530

    Time: 160 ms (8.46%) | Memory: 23.2 MB (16.55%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,28 +1,29 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
+        result = [0]
         m, n = len(grid), len(grid[0])
+        dir = [[1, 0], [0, -1], [-1, 0], [0, 1]]
         vis = [[False] * n for _ in range(m)]
-
-        ans = 0
-
-        def dfs(x, y):
-            nonlocal ans
-
-            vis[x][y] = True
-
-            for dx, dy in [(1,0),(-1,0),(0,1),(0,-1)]:
+        def dfs(x, y, result):
+            for dx, dy in dir:
                 nx = x + dx
                 ny = y + dy
-
-                if nx < 0 or ny < 0 or nx >= m or ny >= n:
-                    ans += 1
-                elif grid[nx][ny] == 0:
-                    ans += 1
-                elif not vis[nx][ny]:
-                    dfs(nx, ny)
-
+                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1 and not vis[nx][ny]:
+                    vis[nx][ny] = True
+                    dfs(nx, ny, result)
+                elif (0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 0 and not vis[nx][ny]):
+                    result[0] += 1
+                elif nx >= m or nx < 0 or ny >= n or ny < 0:
+                    result[0] += 1
+        f = False
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1:
-                    dfs(i, j)
-                    return ans
+                if grid[i][j]:
+                    vis[i][j] = True
+                    dfs(i, j, result)
+                    f = True
+                    break
+            if f:
+                break
+                    
+        return result[0]
